51b9d904d7fad9e5b5889049f7737a500f549782
KevinNhan123 <fakexb0xdr3x@gmail.com>
2023-10-27 10:54:29 -0400

    if

--- Unit 3/IfStatements/GuessingGame.py
+++ Unit 3/IfStatements/GuessingGame.py
@@ -1,5 +1,30 @@
 """
 Name: Kevin Nhan
-Date: September 14th 2023
-Description: Creates a receipt
+Date: October 27 2023
+Description: Guess Game
 """
+#Constants and Variables
+RANDOMNUM = 79
+ALLOWEDGUESSES = 10
+
+userGuess = 0
+
+#Loop
+print("This program will ask the user to guess the random number in 10 tries.\n")
+
+for tries in range(ALLOWEDGUESSES):
+    userGuess = int(input("Please enter a number between 0 to 100: "))
+    while userGuess < 0 or userGuess > 100: #Checks if the input from the user is from 0 to 100
+         userGuess = int(input("\nPlease enter a number between 0 to 100: "))
+    
+    #Checks the value of the guess and compares it to the random number
+    if userGuess > RANDOMNUM:
+        print("\nYour guess was too high!")
+    elif userGuess < RANDOMNUM:
+        print("\nYour guess was too low!")
+    elif userGuess == RANDOMNUM:
+        print("\nYou guess was correct! Here is the amount of tries it took:",tries + 1)
+        break #Ends the loop
+
+if userGuess != RANDOMNUM: #If the user did not guess it in 10 tries
+    print("\nYou did not guess the random number in 10 tries!")
