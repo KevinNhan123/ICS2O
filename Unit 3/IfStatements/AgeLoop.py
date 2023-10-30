461b2747fa7d5c5279bfabb3517304389953c3c8
KevinNhan123 <fakexb0xdr3x@gmail.com>
2023-10-30 10:43:57 -0400

    if

---
+++ Unit 3/IfStatements/AgeLoop.py
@@ -0,0 +1,20 @@
+"""
+Name: Kevin Nhan
+Date: October 27 2023
+Description: Prints even or odd numbers depending on your age
+"""
+
+#Variables
+age = 0
+
+#Asking the user for their age
+age = int(input("Please enter your age: "))
+
+#Loop
+for num in range(age):
+    if age / 2 != age // 2: #If it is odd
+        if (num+1) /2 != (num+1) // 2:
+            print(num+1) #prints out all odd numbers 
+    elif age / 2 == age // 2: #If it is even
+        if (num+1) /2 == (num+1) // 2:
+            print(num+1) #prints out all even numbers 
