06e1db78120ada17213f53975b925a5b87d6b8a1
Kevin Nhan <fakexb0xdr3x@gmail.com>
2023-11-08 17:49:46 -0500

    numberguessingAI

--- Unit 4/TimeAndRandomNum/NumberGuessingAI.py
+++ Unit 4/TimeAndRandomNum/NumberGuessingAI.py
@@ -4,54 +4,45 @@ Date: November 2 2023
 Description: The user picks a random number and the program will guess the number
 """
 #Libraries
-import random, math
+import random
 
 #Variables
 numRange = 0
 randomNum = 0
 
-amtOfDigits = 0
-
 guess = 0
-highGuess = 0
-lowGuess = 0
 
-offset =  0
-amtOfDigits = 0 
+lowRange = 0
+highRange = 0
 
 attempts = 0
+guessedNums = []
 
-#Asks user to choose a random number
-print("This program will guess the number the user enters.\n")
+#Asks user for a range for the bot to start guessing from
 randomNum = int(input("Please enter a random number: "))
-while randomNum < 0 or randomNum > 9999999: #Checks if the user entered something lower than 0 or higher than 9999999
-    randomNum = int(input("Please enter a random number: "))
 
-#Sets the number range so the program knows where to start guessing
-amtOfDigits = int(math.log10(randomNum)) + 1
-
-numRange = int("1"+("0"*(amtOfDigits)))
-offset = numRange / 100
+lowRange = int(input("Please enter the starting guess range: "))
+while lowRange > randomNum: #Makes sure that the lowRange is not greater than the random number
+    lowRange = int(input("Please enter a starting guess range that is less than the random number: "))
+    
+highRange = int(input("Please enter the upper guess range: "))
+while highRange < randomNum: #Makes sure that the highRange is not lower than the random number
+    highRange = int(input("Please enter a upper guess range that is greater than the random number: "))
 
 #Loop
-guess = random.randint(0, numRange)
-
 while guess != randomNum:
-    if guess > randomNum: #If guess is greater than the random number
-        highGuess = guess
-        guess = random.randint(lowGuess,numRange)
-        while guess > highGuess: #Will make sure the guess is lower than the previous guess
-            guess = random.randint(lowGuess,numRange)
-            numRange -= int(numRange / offset)
-            if lowGuess > numRange:
-                numRange = lowGuess
-            print(guess,lowGuess,numRange)
-        attempts += 1 #Only will add an attempt after the loop
-    elif guess < randomNum:
-        lowGuess = guess
-        numRange += int(offset)
-        guess = random.randint(lowGuess,numRange)
-        attempts += 1
+    attempts += 1
+    guess = random.randint(lowRange,highRange)
+    while guess > randomNum: #If the guess is greater than the random number, it will keep guessing a new random number until it finds a number that is lower than the random number
+        guess = random.randint(lowRange,highRange)
+    
+    if guess < randomNum: #If the guess is lower than the random number, it will set the lowrange to the guess and find a new random number
+        lowRange = guess+1
+        guess = random.randint(lowRange,highRange)
+    
+    guessedNums.append(guess)
 
-
-print("Here is your random number:",guess,"Attempts:",attempts)
+#Outputs result
+print("\nHere is your random number:", guess,"\nTo guess your number, I took",attempts,"attempt(s)")
+print("\nHere are the numbers I guessed:")
+print(guessedNums)
