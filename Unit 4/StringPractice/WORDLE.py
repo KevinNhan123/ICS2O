c69d0e2d98cedc71559988d21c3d7d210d9b4f3d
Kevin Nhan <fakexb0xdr3x@gmail.com>
2023-11-14 21:56:36 -0500

    strings

--- Unit 4/StringPractice/WORDLE.py
+++ Unit 4/StringPractice/WORDLE.py
@@ -4,15 +4,24 @@ Date: November 14 2023
 Description: This is a simplified version of WORDLE
 """
 
+#Libraries
+import random
+
 #Constants and Variables
-WORD = "MONTY" #Has to be five letters
 AMTOFTRIES = 5
+
+words = ["MONTY","DATAS","EAGER","DAIRY","CABOB","BABOO","FACET","FACES","GABLE","HABIT","ICIER","JACKS","JADED","KABAB","LACED","PACAS","PACED","CHESS","HORSE","QUEEN","UDDER","XENON","TREES","GOATS","WACKO","KEVIN","WACKS","TABER","VAGUS","ROOTS","CRAMS","TABLE","CHAIR","PANDA"]
+
+word = 0
 
 guess = 0
 restart = "Y"
 
 #Loop
 while restart == "Y":
+    #Selects random word from list
+    word = words[random.randint(0,len(words)-1)]
+    
     #Gives the user context of program
     print("This is a simplified version of WORDLE! Try to guess the word in 5 tries!\n")
     print("-"*80)
@@ -24,19 +33,19 @@ while restart == "Y":
             guess = input("\nPlease enter a guess (Make sure it is 5 letters and is a word): ").upper()
     
         #Compares guess to word
-        for letter in range(len(WORD)):
-            if WORD[letter] == guess[letter]:
-                print(WORD[letter], end = "")
+        for letter in range(len(word)):
+            if word[letter] == guess[letter]:
+                print(word[letter], end = "")
             else:
                 print("-", end = "")
     
         #Checks if guess == word
-        if guess == WORD:
+        if guess == word:
             print("\nCongrats! You guessed the word!")
             break
 
     #End of WORDLE
-    if guess != WORD:
+    if guess != word:
         print("\nYou did not guess the word in 5 tries!")
 
     #Prompts the user if they would like to restart the program
