3e9fc0cf3204ae9f553aa83e887c439951a1843e
Kevin Nhan <fakexb0xdr3x@gmail.com>
2023-11-23 23:39:44 -0500

    quiz

--- Unit 4/QuizSummative.py
+++ Unit 4/QuizSummative.py
@@ -9,37 +9,41 @@ import time
 
 #? Constants and Variables
 MAXTRIES = 2
+OPTIONS = "1234"
 
 quizRun = True
 
 #Information for file
-correct = 0
+correct = []
+incorrect = []
+
 questionsAnswered = 0
 
 #User related
-options = "1234"
 prompt = 0
+
+extraTries = False
 
 questions = [
     ["Who invented Python?", #! First Question
     ["Guido van Rossum", "Monty Python", "Mr. Ribeiro", "Microsoft"], #! Multiple choices
-    0 #! Answer
+    "1" #! Answer
     ], #-------------------------------------------------------------------------------------
     
     ["When the user enters an input, what is the initial data type?", #* Second Question
     ["String", "Undefined", "Whatever the user entered", "Integer"], #* Multiple choices
-    0 #* Answer
+    "1" #* Answer
     ], #-------------------------------------------------------------------------------------
     
     ["What year was Python created?", #? Third Question
      ["1967", "1991", "1979", "2001"], #? Multiple choices
-     1, #? Answer
+     "2", #? Answer
      MAXTRIES #? Allowed tries
      ], #-------------------------------------------------------------------------------------
     
     ["What is wrong with the following code?\n\n guess = int(input(\"Please enter a number: \"))\n while (not guess.isnumeric()):\n\tprint(\"Your guess was not a number!\")", #! Fourth Question
      ["All of the below", "The loop is going to run regardless of what the user enters", "The code is formatted incorrectly", "The user might not enter a number and the program will crash"], #! Multiple choices
-     3, #! Answer
+     "4", #! Answer
      MAXTRIES #! Allowed tries
     ], #-------------------------------------------------------------------------------------
     
@@ -74,11 +78,55 @@ while quizRun == True:
             for option in range (len(questions[question][1])):
                 time.sleep(0.5)
                 print(str(option+1)+".", questions[question][1][option]) #Prints out all the options for the question
-        
-            #Asks user for prompt
-            prompt = input("\nPlease chose an option: ")
-            while not prompt in options: #Checks if prompt is not one of the options
-                print("Your choice was invaild!")
-                prompt = input("Please chose an option: ")
+
+            #Checks if the question allows for extra tries
+            try: #This is used because for some of the questions in the list, MAXTRIES is not defined in the list so it will output an error. This is like a safety net, if there is no MAXTRIES in the list, it will set extraTries to False
+                if questions[question].index(MAXTRIES) == 3:
+                    extraTries = True
+            except ValueError:
+                extraTries = False
             
-            #TODO check if there are extra tries allowed and check answer
+            #Asks user for prompt
+            if extraTries == True: #If the question is one of the harder multiple choice questions
+                print("\nThis is a hard multiple choice question! You get two tries!")
+                for i in range(MAXTRIES):
+                    prompt = input("\nPlease chose an option: ")
+                    while not prompt in OPTIONS: #Checks if prompt is not one of the options
+                        print("Your choice was invaild!")
+                        prompt = input("Please chose an option: ")
+                        
+                    #Checks if the prompt matches up with the answer
+                    if prompt == questions[question][2]:
+                        correct.append(question) #Adds question to correct list
+                        print("\nNice! You got the question correct!")
+                        break
+                    else: #If the user did not answer the question correctly (If the question is not in the correct list)
+                        print("\nIncorrect!")
+                    
+                if prompt != questions[question][2]: #After the two tries, if the last prompt was incorrect then the question is added to the incorrect list
+                    incorrect.append(question)
+                
+            else: #If it is regular multiple choice
+                prompt = input("\nPlease chose an option: ")
+                while not prompt in OPTIONS: #Checks if prompt is not one of the options
+                    print("Your choice was invaild!")
+                    prompt = input("Please chose an option: ")
+                
+                #Checks if it is correct
+                if prompt == questions[question][2]:
+                    correct.append(question) #Adds question to correct list
+                    print("\nNice! You got the question correct!")
+                else: #If it is incorrect
+                    incorrect.append(question)
+                    print("\nIncorrect!")
+        
+        else: #If the question is a fill-in-the-blanks
+            prompt = input("\nPlease enter an answer: ").lower()
+            
+            #Checks if prompt is correct
+            if prompt == questions[question][1]:
+                correct.append(question)
+                print("\nNice! You got the question correct!")
+            else: #If it is incorrect
+                incorrect.append(question)
+                print("\nIncorrect!")
