c8b9e85d3477670b322cdc3587cb4eb69967aab8
KevinNhan123 <fakexb0xdr3x@gmail.com>
2023-12-14 11:31:44 -0500

    pygame

---
+++ Unit 5/FunProject/Settings.py
@@ -0,0 +1,8 @@
+"""
+Name: Kevin Nhan
+Date: December 14th 2023
+Description: Settings
+"""
+
+#Game set up
+size = (700,500)
---
+++ Unit 5/FunProject/UsingClasses.py
@@ -0,0 +1,9 @@
+"""
+Name: Kevin Nhan
+Date: December 14th 2023
+Description: Practicing classes
+"""
+
+#Classes
+class Character:
+    
